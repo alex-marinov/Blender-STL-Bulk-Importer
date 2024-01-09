747fb42bf9678c06687867bee9961c9a346cdfdd
Alex Marinov <alex@diylab.uk>
2024-01-09 09:55:03 +0000

    initial script and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Blender-STL-Bulk-Importer
+
+## Description
+
+This Python script is used to create collections and import STL files in Blender using the bpy (Blender Python) library. It recursively traverses a specified root folder, creates collections for subfolders, and imports STL files into the corresponding collections.
+
+## Usage
+
+Replace 'root-folder-path' with the actual root folder path in the script.
+Run the script in Blender's scripting interface or from the command line using Blender's --python option.
+
+Feel free to customize this script as needed for your project.
+If you have any further questions or need additional assistance, please feel free to ask!
---
+++ bulk_stl_import.py
@@ -0,0 +1,34 @@
+import bpy
+import os
+
+# Root folder path
+root_folder = 'root-folder-path'
+
+def create_collections(root, parent_collection):
+    for item in os.listdir(root):
+        item_path = os.path.join(root, item)
+        if os.path.isdir(item_path):
+            # Create a collection for the current folder
+            collection = bpy.data.collections.new(item)
+            parent_collection.children.link(collection)
+            # Recursively create collections for subfolders
+            create_collections(item_path, collection)
+            # Import STL files within the current collection
+            import_stl_files(item_path, collection)
+
+def import_stl_files(root, parent_collection):
+    for file in os.listdir(root):
+        file_path = os.path.join(root, file)
+        if file.endswith(".stl"):
+            # Import the STL file
+            bpy.ops.import_mesh.stl(filepath=file_path)
+            # Move the imported object to the parent collection
+            obj = bpy.context.selected_objects[0]
+            parent_collection.objects.link(obj)
+
+# Create a main collection for the root folder
+main_collection = bpy.data.collections.new(os.path.basename(root_folder))
+bpy.context.scene.collection.children.link(main_collection)
+
+# Recursively create collections and import STL files
+create_collections(root_folder, main_collection)
